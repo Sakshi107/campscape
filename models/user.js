489744b1327f020a197a910eb6f5272a03f69f62
Sakshi Shelar <46996938+Sakshi107@users.noreply.github.com>
2020-06-18 21:07:41 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,3 @@
-// var mongoose=require('mongoose');
-// var passwordLocalMongoose=require('passport-local-mongoose');
-
-// var UserSchema=new mongoose.Schema({
-// 	username:String,
-// 	password:String
-// });
-
-// UserSchema.plugin(passwordLocalMongoose);
-
-// module.exports=mongoose.model('User',UserSchema);
-
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
