9242b25f646a7a062b8e6d734d3dd7902eaea75d
Sakshi Shelar <46996938+Sakshi107@users.noreply.github.com>
2020-06-18 22:22:16 +0530

    Update seeds.js

--- seeds.js
+++ seeds.js
@@ -55,7 +55,6 @@ var data = [
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
 ]
- 
 function seedDB(){
    //Remove all campgrounds
    Campground.remove({}, function(err){
